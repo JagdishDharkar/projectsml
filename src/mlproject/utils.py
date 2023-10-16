b873413db1ef60d191efa882089050e3fc1096c1
Jagdish Dharkar <jagdishd413@gmail.com>
2023-10-16 14:09:06 +0530

    Data ingestion

---
+++ src/mlproject/utils.py
@@ -0,0 +1,42 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+import pymysql
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Get database connection information from environment variables
+host = os.getenv("host")
+user = os.getenv("user")
+password = os.getenv("password")
+db = os.getenv('db')
+
+def read_sql_data():
+    logging.info("Reading SQL database started")
+    try:
+        # Establish a connection to the MySQL database
+        mydb = pymysql.connect(
+            host=host,
+            user=user,
+            password=password,
+            db=db
+        )
+        logging.info("Connection Established", mydb)
+
+        # Execute a SQL query to retrieve data from the "students" table
+        query = 'SELECT * FROM students'
+        df = pd.read_sql_query(query, mydb)
+
+        # Print the first few rows of the DataFrame (for debugging purposes)
+        print(df.head())
+
+        return df
+
+    except Exception as ex:
+        raise CustomException(ex)
+
+# Usage: Call read_sql_data() to retrieve data from the MySQL database
